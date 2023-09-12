7c2c9b919033348b811e7ceb90096be762246d5f
스터디하자 <hmin5015@gmail.com>
2023-09-11 22:49:29 -0700

    Add EditPost function

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Nav from './Nav';
 import Footer from './Footer';
 import Home from './Home';
 import NewPost from './NewPost';
+import EditPost from './EditPost';
 import PostPage from './PostPage';
 import About from './About';
 import Missing from './Missing';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { format } from 'date-fns';
+import api from './api/posts';
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -16,7 +18,29 @@ function App() {
   const [searchResults, setSearchResults] = useState([]);
   const [postTitle, setPostTitle] = useState('');
   const [postBody, setPostBody] = useState('');
+  const [editTitle, setEditTitle] = useState('');
+  const [editBody, setEditBody] = useState('');
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await api.get('/posts');
+        setPosts(response.data);
+      } catch (error) {
+        if (error.response) {
+          // Not in the 200 response range
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else {
+          console.log(`Error: ${error.message}`);
+        }
+      }
+    }
+
+    fetchPosts();
+  }, []);
 
   useEffect(() => {
     const filteredResults = posts.filter(post => 
@@ -26,24 +50,48 @@ function App() {
       setSearchResults(filteredResults.reverse());
   }, [posts, search]);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
-    const datetime = format(new Date(), 'MMMM dd, yyyy pp');
-    const newPost = { id, title: postTitle, datetime, body: postBody };
-    const allPosts = [...posts, newPost];
-    setPosts(allPosts);
-    setPostTitle('');
-    setPostBody('');
-    navigate('/');
+    const dateTime = format(new Date(), 'MMMM dd, yyyy pp');
+    const newPost = { id, title: postTitle, dateTime, body: postBody };
+
+    try {
+      const response = await api.post('/posts', newPost);
+      const allPosts = [...posts, response.data];
+      setPosts(allPosts);
+      setPostTitle('');
+      setPostBody('');
+      navigate('/');      
+    } catch (error) {
+      console.log(`Error: ${error.message}`);
+    }
   }
 
-  const handleDelete = (id) => {
-    // set current posts without the selected posts which will be deleted
-    const postsList = posts.filter(post => post.id !== id);
-    setPosts(postsList);
-    navigate('/');
+  const handleEdit = async (id) => {
+    const dateTime = format(new Date(), 'MMMM dd, yyyy pp');
+    const updatedPost = { id, title: editTitle, dateTime, body: editBody };
+    try {
+      const response = await api.put(`/posts/${id}`, updatedPost);
+      setPosts(posts.map(post => post.id === id ? { ...response.data } : post));
+      setEditTitle('');
+      setEditBody('');
+      navigate('/');
+    } catch (error) {
+      console.log(`Error: ${error.message}`);
+    }
+  }
+
+  const handleDelete = async (id) => {
+    try {
+      await api.delete(`/posts/${id}`);
+      const postsList = posts.filter(post => post.id !== id);
+      setPosts(postsList);
+      navigate('/');
+    } catch (error) {
+      console.log(`Error: ${error.message}`);
+    }
   }
 
   return (
@@ -64,6 +112,19 @@ function App() {
               setPostTitle={setPostTitle} 
               postBody={postBody} 
               setPostBody={setPostBody}
+            />
+          }
+        />
+        <Route 
+          path="/edit/:id" 
+          element={
+            <EditPost 
+              posts={posts}
+              handleEdit={handleEdit} 
+              editTitle={editTitle} 
+              setEditTitle={setEditTitle} 
+              editBody={editBody} 
+              setEditBody={setEditBody}
             />
           }
         />
