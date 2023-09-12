2d12f2ebb7a74441181a800fa692b8eaad7d9015
스터디하자 <hmin5015@gmail.com>
2023-09-12 07:42:12 -0700

    Modify Header to show the device icon by window size using useWindowSize hook

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { FaLaptop, FaTableAlt, FaMobileAlt } from 'react-icons/fa';
+import { FaLaptop, FaTablet, FaMobileAlt } from 'react-icons/fa';
 
 const Header = ({ title, width }) => {
   return (
     <header className="Header">
       <h1>{title}</h1>
+      {width < 768 ? <FaMobileAlt /> : width < 992 ? <FaTablet /> : <FaLaptop />}
     </header>
   )
 }
